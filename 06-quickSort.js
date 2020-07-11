5419988bf8b4f1945122389f89fb7289d03b5d25
ZDT-PC <dev@dthappy.com>
2020-07-11 21:07:09 +0800

    Update quickSort Return

--- 06-quickSort.js
+++ 06-quickSort.js
@@ -7,45 +7,42 @@
  * 注：快排中递归操作的是数组的下标（即首、尾），和归并不同，归并左右是数组对象
  * @see https://www.runoob.com/w3cnote/quick-sort.html
  */
-(function(){
-    
-    const ascSort = function(array){
-        return quickSort(array,0,array.length-1);
+(function () {
+
+    const ascSort = function (array) {
+        return quickSort(array, 0, array.length - 1);
     }
 
-    const quickSort = function(array,left,right){
-        if(left<right){
-            var pivotIndex = partition(array,left,right);
-            quickSort(array,left,pivotIndex-1);
-            quickSort(array,pivotIndex+1,right);
-            return array;
-        }else{
-            //left 和 right 相等，说明只有一个元素
-            return array;
+    const quickSort = function (array, left, right) {
+        if (left < right) {
+            var pivotIndex = partition(array, left, right);
+            quickSort(array, left, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, right);
         }
+        return array;
     }
 
     //主要目的为求基准在什么位置，左边都比其小，右边都比其大
     //难点在于如何将pivotInde找出：思路在于从右边找小的挖坑，再从左边找大的填坑
-    const partition = function(array,left,right){
+    const partition = function (array, left, right) {
         let pivotIndex = left;
         let pivot = array[pivotIndex];
-        while(right>left){
-            while(array[right]>pivot){
+        while (right > left) {
+            while (array[right] > pivot) {
                 right--;
             }
             swap(array, pivotIndex, right);
             pivotIndex = right;
-            while(array[left]<pivot){
+            while (array[left] < pivot) {
                 left++;
             }
             swap(array, pivotIndex, left);
             pivotIndex = left;
         }
-        return pivotIndex;   
+        return pivotIndex;
     }
 
-    const swap = function(array, i,j){
+    const swap = function (array, i, j) {
         let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
