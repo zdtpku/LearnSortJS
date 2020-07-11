a522464113cbd03fe06ff8ee72f6dc7b1b0bb5ee
ZDT-PC <dev@dthappy.com>
2020-07-11 14:45:51 +0800

    add 01-bubble-sort.js

---
+++ 01-bubbleSort.js
@@ -0,0 +1,24 @@
+/**
+ * Bubble Sort 
+ * 
+ */
+(function () {
+
+    const sort = function (array) {
+        for (let i = 0; i < array.length-1; i++) {
+            for (let j = 0; j < array.length-i-1; j++) {
+                if (array[j] > array[j + 1]) {
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+
+    const testArray = [2,20,1,56,21,3,9,2,6,1,99,34];
+    let sortedArray = sort(testArray);
+    console.log(sortedArray);
+
+})();
